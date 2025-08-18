7125292ee9f8b0d8ae51a73b5c702b3151d6abce
muiz Salam <salammulad@gmail.com>
2025-08-18 15:47:26 +0100

    removed

--- components/featured-albums.tsx
+++ components/featured-albums.tsx
@@ -6,7 +6,7 @@ import MusicPlayer from "@/components/music-player";
 export default function FeaturedAlbums() {
   const albums = [
     {
-      title: "Sacred Winds",
+      title: "Restoring the Ancient Ruins ",
       year: "2019",
       image: "/images/wind2.jpg",
     },
